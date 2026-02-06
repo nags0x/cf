5602788688d597acacd65893126c608b593d7f49
Nags <nagpichikaganesh@gmail.com>
2026-02-06 23:58:17 +0530

    50a.cpp

---
+++ 50a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int M, N;
+    
+    if (cin >> M >> N) {
+        int result = (M * N) / 2;
+        
+        cout << result << endl;
+    }
+    
+    return 0;
+}
